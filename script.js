f56520fd022427d6eb04e29e2f8ec71578edb4f9
Herbert Cuyugan <herbert.cuyugan@pentanasolutions.com>
2023-07-10 22:23:36 +1000

    added javascript functionality

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
 // learning from - https://www.youtube.com/watch?v=oaliV2Dp7WQ
+// https://unsplash.com/
+
+// another one - https://www.youtube.com/watch?v=E4znbZgUWzA
+
+const accessKey = "3YuM79ILaiFxw01bGXUijqMDYu9uuao6parTjRQRPOE";
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+
+async function searchImage() {
+  inputData = inputEl.value;
+  // dynamic url below
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const results = data.results;
+
+  if (page === 1) {
+    searchResults.innerHTML = "";
+  }
+
+  results.map((result) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result");
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    image.alt = result.alt_description;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.textContent = result.alt_description;
+
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResults.appendChild(imageWrapper);
+  });
+
+  page++;
+  if (page > 1) {
+    showMore.style.display = "block";
+  }
+}
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  page = 1;
+  searchImage();
+});
+
+showMore.addEventListener("click", () => {
+  searchImage();
+});
